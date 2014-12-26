46696e9a2542112e7a601d4cc1297891659e4b74
floriangreinacher <post@floriangreinacher.de>
2014-12-26 13:08:02 +0100

    Add musicians

---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,11 @@
+musician(john).
+musician(jim).
+musician(jane).
+
+instrument(john, guitar).
+instrument(jim, drums).
+instrument(jane, piano).
+
+genre(john, heavyMetal).
+genre(jim, rock).
+gerne(jane, classic).
