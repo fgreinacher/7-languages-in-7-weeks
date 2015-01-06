1c2b2927c7e9a4c16149567c962b495817baa5be
floriangreinacher <post@floriangreinacher.de>
2015-01-06 01:14:17 +0100

    Some more list ops

--- prolog/day2/list.pl
+++ prolog/day2/list.pl
@@ -1,8 +1,21 @@
 :- include('concat.pl').
 
-recRev([], []).
-recRev([Head|Tail], Result) :- recRev(Tail, RevTail), append(RevTail, [Head], Result).
+myRev(List, Result) :- doAccRev(List, [], Result).
+doRev([], Acc, Acc).
+doRev([Head|Tail], Acc, Result) :- doRev(Tail, [Head|Acc], Result).
 
-accRev(List, Result) :- doAccRev(List, [], Result).
-doAccRev([], Acc, Acc).
-doAccRev([Head|Tail], Acc, Result) :- doAccRev(Tail, [Head|Acc], Result). 
+myMin(List, Min) :- List = [Head|_], doMin(List, Head, Min).
+doMin([Head|Tail], CurMin, Min) :- Head < CurMin, doMin(Tail, Head, Min).
+doMin([Head|Tail], CurMin, Min) :- Head >= CurMin, doMin(Tail, CurMin, Min).
+doMin([], CurMin, CurMin).
+
+mySort(List, Sorted) :- doSort(List, [], Sorted).
+doSort([], Acc, Acc).
+doSort([Head|Tail], Acc, Sorted):-
+    pivoting(Head, Tail, Left, Right),
+    doSort(Left, Acc, SortedLeft),
+    doSort(Right, [Head|SortedLeft], Sorted).
+
+pivoting(_,[],[],[]).
+pivoting(H,[X|T],[X|L],G) :- X =< H, pivoting(H,T,L,G).
+pivoting(H,[X|T],L,[X|G]) :- X > H, pivoting(H,T,L,G).
