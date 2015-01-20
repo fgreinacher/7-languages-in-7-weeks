79a7602274b002a5042f155230cffc082cc87763
floriangreinacher <post@floriangreinacher.de>
2015-01-20 22:50:08 +0100

    Improve tic tac toe

--- scala/day1/tictactoe.scala
+++ scala/day1/tictactoe.scala
@@ -12,41 +12,51 @@ case object Draw extends State
 case class Won(player: Player) extends State
 
 class Board(cells: ( (Cell, Cell, Cell), (Cell, Cell, Cell), (Cell, Cell, Cell) )) {
-    def row1() = Iterable(cells._1._1, cells._1._2, cells._1._3)
-    def row2() = Iterable(cells._2._1, cells._2._2, cells._2._3)
-    def row3() = Iterable(cells._3._1, cells._3._2, cells._3._3)
+    val row1 = List(cells._1._1, cells._1._2, cells._1._3)
+    val row2 = List(cells._2._1, cells._2._2, cells._2._3)
+    val row3 = List(cells._3._1, cells._3._2, cells._3._3)
     
-    def col1() = Iterable(cells._1._1, cells._2._1, cells._3._1)
-    def col2() = Iterable(cells._1._2, cells._2._2, cells._3._2)
-    def col3() = Iterable(cells._1._3, cells._2._3, cells._3._3)
+    val col1 = List(cells._1._1, cells._2._1, cells._3._1)
+    val col2 = List(cells._1._2, cells._2._2, cells._3._2)
+    val col3 = List(cells._1._3, cells._2._3, cells._3._3)
     
-    def dia1() = Iterable(cells._1._1, cells._2._2, cells._3._3)
-    def dia2() = Iterable(cells._3._1, cells._2._2, cells._1._3)
+    val dia1 = List(cells._1._1, cells._2._2, cells._3._3)
+    val dia2 = List(cells._3._1, cells._2._2, cells._1._3)
 
-    def lines() = Iterable(row1(), row2(), row3(), col1(), col2(), col3(), dia1(), dia2())
-
+    val lines = List(row1, row2, row3, col1, col2, col3, dia1, dia2)
+   
     def occupies(player: Player, cells: Iterable[Cell]) = cells.forall(_ == OccupiedCell(player))
-
-    def won(player: Player) = lines().exists(occupies(player, _))
+    def won(player: Player) = lines.exists(occupies(player, _))
 
     def state() : State = {
         if(won(Player1)) return Won(Player1)
         else if(won(Player2)) return Won(Player2)
-        else return Running
+        else if(lines.flatten.contains(EmptyCell)) return Running
+        else return Draw
     }
 
     override def toString() = 
         Iterable(
-            row1().mkString(" | "),
-            row2().mkString(" | "),
-            row3().mkString(" | ")
+            row1.mkString(" | "),
+            row2.mkString(" | "),
+            row3.mkString(" | ")
         ).mkString("\n")
 }
 
-var cells = (
+var player1Won = new Board ((
     (OccupiedCell(Player1),     EmptyCell,              OccupiedCell(Player1)),
     (EmptyCell,                 OccupiedCell(Player1),  EmptyCell),
     (EmptyCell,                 OccupiedCell(Player2),  OccupiedCell(Player1))
-)
+))
 
-var board = new Board(cells)
+var draw = new Board((
+    (OccupiedCell(Player1), OccupiedCell(Player2), OccupiedCell(Player1)),
+    (OccupiedCell(Player1), OccupiedCell(Player2), OccupiedCell(Player2)),
+    (OccupiedCell(Player2), OccupiedCell(Player1), OccupiedCell(Player1))
+))
+
+var running = new Board((
+    (EmptyCell, OccupiedCell(Player2), OccupiedCell(Player1)),
+    (OccupiedCell(Player1), OccupiedCell(Player2), OccupiedCell(Player2)),
+    (OccupiedCell(Player2), OccupiedCell(Player1), OccupiedCell(Player1))
+))
